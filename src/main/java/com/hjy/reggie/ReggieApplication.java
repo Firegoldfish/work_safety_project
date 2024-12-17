9abfb21560a9b79f09100cd13d76ecb588f8ae20
Bill Madison <Firegoldfish@outlook.com>
2024-12-17 21:50:22 +0800

    导入页面，设置映射

---
+++ src/main/java/com/hjy/reggie/ReggieApplication.java
@@ -0,0 +1,15 @@
+package com.hjy.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})    //
+@Slf4j //可以使用log
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("启动成功！！！");
+    }
+}
