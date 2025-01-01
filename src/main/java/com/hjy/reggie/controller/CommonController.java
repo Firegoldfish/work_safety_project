8285840f0f30b389488eeb49b5f66866e60a9a41
Bill Madison <Firegoldfish@outlook.com>
2025-01-02 00:36:31 +0800

    文件下载

--- src/main/java/com/hjy/reggie/controller/CommonController.java
+++ src/main/java/com/hjy/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.hjy.reggie.controller;
 import com.hjy.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController
@@ -36,5 +41,24 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.success(uuid);
+    }
+    @GetMapping("/download")
+    public void download(HttpServletResponse response, String name) throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(basePath+ name));
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+            int len = 0;
+            byte[] buffer = new byte[1024];
+            while ((len = fis.read(buffer))!=-1){
+                outputStream.write(buffer,0,len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fis.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
