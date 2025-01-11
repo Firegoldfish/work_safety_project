dac0aea323684b6c6b3f8206ac2e08e4e2e24b66
Bill Madison <Firegoldfish@outlook.com>
2025-01-11 21:36:35 +0900

    短信业务

--- src/main/java/com/hjy/reggie/utils/SMSUtils.java
+++ src/main/java/com/hjy/reggie/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
      * @param param 参数
      */
     public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5t8ttCw3Vzi6pKx6chsA", "5lesXbk5M8V5XJIX0apttLkZQQ7EWK");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", System.getenv("ALIBABA_CLOUD_ACCESS_KEY_ID"), System.getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET"));
         IAcsClient client = new DefaultAcsClient(profile);
 
         SendSmsRequest request = new SendSmsRequest();
